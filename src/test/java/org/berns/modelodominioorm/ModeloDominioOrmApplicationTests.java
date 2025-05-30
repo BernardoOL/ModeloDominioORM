a69b78ce1bc65049bd99b20977dc30469bac225e
BernardoLopes <bernardomak42@gmail.com>
2025-05-30 19:44:30 -0300

    Created

---
+++ src/test/java/org/berns/modelodominioorm/ModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.berns.modelodominioorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloDominioOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
